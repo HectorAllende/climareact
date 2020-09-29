640a4e60faa3e31a3299c94477a4ae1c0e39fd5e
HecAllende <54869462+HectorAllende@users.noreply.github.com>
2020-09-28 21:49:52 -0300

    clima

---
+++ precache-manifest.34722e53c1385813ae9f774c242a9e44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5175ffeeff55c0cab8b0af670a5fbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6039d3868db30c0589a8",
+    "url": "/static/css/main.99037991.chunk.css"
+  },
+  {
+    "revision": "537430fb85c3bf217ce5",
+    "url": "/static/js/2.d814037f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d814037f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6039d3868db30c0589a8",
+    "url": "/static/js/main.559854e1.chunk.js"
+  },
+  {
+    "revision": "1ee3f02f01f46ffe1680",
+    "url": "/static/js/runtime-main.e21afe43.js"
+  }
+]);
